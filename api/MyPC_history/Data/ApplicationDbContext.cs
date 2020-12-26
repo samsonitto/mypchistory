a433ea5b8c9304547dfed25de4c90f3900bd863f
M3156 <m3156@student.jamk.fi>
2020-12-26 16:37:01 +0200

    first

---
+++ api/MyPC_history/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPC_history.Data
+{
+    public class ApplicationDbContext
+    {
+        private IConfiguration _config;
+
+        public ApplicationDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(_config.GetConnectionString("DBConnection")));
+        }
+    }
+}
